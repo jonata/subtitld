3b421ff02b68023ac98c2287a176bb7c62b26877
Jonatã Bolzan Loss <jonata@jonata.org>
2020-04-14 16:08:05 -0300

    Start implementing history

---
+++ modules/subtitles.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from bisect import bisect
+
+def add_subtitle(subtitles=[], position=0.0, duration=5.0, text=''):
+    current_index = 0
+
+    subt = [item[0] for item in subtitles]
+    index = bisect(subt, position)
+
+
+    if index and subtitles[index-1][0] + subtitles[index-1][1] > position:
+        subtitles[index-1][1] -= (subtitles[index-1][0] + subtitles[index-1][1]) - position
+
+    if len(subtitles) - 1 > index and subtitles[index][0] - position < duration:
+        duration = subtitles[index][0] - position
+
+    subtitles.insert(index, [position, duration, text])
+
+    return subtitles[current_index]
+
+def remove_subtitle(subtitles=[], selected_subtitle=False):
+    if selected_subtitle:
+        subttiles.remove(selected_subtitle)
+    return subtitles
+
+def slice_subtitle(subtitles=[], selected_subtitle=False, position=0.0, last_text='', next_text=''):
+    if selected_subtitle and position > selected_subtitle[0] and position < (selected_subtitle[0] + selected_subtitle[1]):
+        index = subtitles.index(selected_subtitle)
+
+        if position > subtitles[index][0] and position < (subtitles[index][0] + subtitles[index][1]):
+            position_to_cut = position
+        else:
+            position_to_cut = (subtitles[index][0] + subtitles[index][1])/2
+
+        new_duration = (subtitles[index][0] + subtitles[index][1]) - position_to_cut
+
+        subtitles[index][1] = position_to_cut - subtitles[index][0] - 0.001
+        subtitles[index][2] = last_text
+
+        return add_subtitle(subtitles=subtitles, position=position_to_cut, duration=new_duration, text=next_text)
