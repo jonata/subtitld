d18f7c2079d4eebf7f568d68a2a0b8815230f02e
Jonatã Bolzan Loss <jonata@jonata.org>
2020-06-04 23:53:55 -0300

    Code cleanup and improvement on QOpenGLWidget

--- modules/paths.py
+++ modules/paths.py
@@ -39,7 +39,7 @@ elif sys.platform == 'win32' or os.name == 'nt':
     STARTUPINFO.dwFlags |= subprocess.STARTF_USESHOWWINDOW
     STARTUPINFO.wShowWindow = subprocess.SW_HIDE
 else:
-    REAL_PATH_HOME = subprocess.Popen(['getent', 'passwd', str(os.getuid())], stdout=subprocess.PIPE).stdout.read().decode().split(':')[5]
+    REAL_PATH_HOME = subprocess.Popen(['getent', 'passwd', str(os.getuid())], stdout=subprocess.PIPE).stdout.read().decode().split(': ')[5]
     if not os.path.isdir(os.path.join(PATH_HOME, '.config')):
         os.mkdir(os.path.join(PATH_HOME, '.config'))
     PATH_SUBTITLD_USER_CONFIG = os.path.join(PATH_HOME, '.config', 'subtitld')
@@ -70,12 +70,12 @@ def get_graphics_path(filename):
 
 LIST_OF_SUPPORTED_VIDEO_EXTENSIONS = (('.mp4', '.mkv', '.mov', '.mpg', '.webm', '.ogv'))
 LIST_OF_SUPPORTED_SUBTITLE_EXTENSIONS = {
-    'SRT': {'description':'SubRip Subtitle format', 'extensions':['srt']},
-    'DFXP': {'description':'Subtitle format', 'extensions':['ttml', 'dfxp']},
-    'SAMI': {'description':'Subtitle format', 'extensions':['smi', 'sami']},
-    'SCC': {'description':'Subtitle format', 'extensions':['scc']},
-    'VTT': {'description':'Subtitle format', 'extensions':['webvtt', 'vtt']},
-    'ASS': {'description':'SubStation Alpha Subtitle format', 'extensions':['ass', 'ssa']},
-    'SBV': {'description':'Subtitle format', 'extensions':['sbv']},
-    'XML': {'description':'Subtitle format', 'extensions':['xml']}
+    'SRT': {'description': 'SubRip Subtitle format', 'extensions': ['srt']},
+    'DFXP': {'description': 'Subtitle format', 'extensions': ['ttml', 'dfxp']},
+    'SAMI': {'description': 'Subtitle format', 'extensions': ['smi', 'sami']},
+    'SCC': {'description': 'Subtitle format', 'extensions': ['scc']},
+    'VTT': {'description': 'Subtitle format', 'extensions': ['webvtt', 'vtt']},
+    'ASS': {'description': 'SubStation Alpha Subtitle format', 'extensions': ['ass', 'ssa']},
+    'SBV': {'description': 'Subtitle format', 'extensions': ['sbv']},
+    'XML': {'description': 'Subtitle format', 'extensions': ['xml']}
 }
