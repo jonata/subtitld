10c906d3b0d8aa017785253f19823f73b876cf57
Jonata Bolzan Loss <jonata@jonata.org>
2022-06-23 16:03:13 -0300

    Debug snap

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ setup_requires = ['setuptools']
 install_requires = [
     'PySide6==6.2.4',
     'pyopengl',
-    'ffms2==0.4.3',
+    'ffms2==0.4',
     'python-mpv',
     'numpy',
     'cffi',
