ea299b68d0689cc811efe17510fa2680f19df555
Jonata Bolzan Loss <jonata@jonata.org>
2022-06-24 12:10:02 -0300

    Debug captionstransformer

--- subtitld/captionstransformer/script.py
+++ subtitld/captionstransformer/script.py
@@ -6,26 +6,26 @@ from captionstransformer.registry import REGISTRY
 
 def read_options():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:f:o:g:") 
+        opts, args = getopt.getopt(sys.argv[1:], "i:f:o:g:")
     except getopt.GetoptError:
         usage()
         sys.exit(2)
 
     options = {}
     valid = 0
-    print opts
+    print(opts)
     for option in opts:
         if option[0] == '-i':
             path = option[1]
             if not os.path.exists(path):
-                print "not an existing file: %s" % path
+                print("not an existing file: %s" % path)
                 return
             options['input'] = path
             valid += 1
         elif option[0] == '-f':
             format = option[1]
             if format not in REGISTRY:
-                print "not supported format: %s" % format
+                print("not supported format: %s" % format)
                 return
             options['input_format'] = format
             valid += 1
@@ -36,13 +36,13 @@ def read_options():
         elif option[0] == '-g':
             format = option[1]
             if format not in REGISTRY:
-                print "not supported format: %s" % format
+                print("not supported format: %s" % format)
                 return
             options['output_format'] = format
             valid += 1
 
     if len(options.keys()) != 4:
-        print "arguments are all required"
+        print("arguments are all required")
         return
 
     return options
