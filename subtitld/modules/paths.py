4f12287a6394567cc0f4473ff20f065fe40d36d4
Jonata Bolzan Loss <jonata@jonata.org>
2022-10-11 21:42:39 -0300

    Debug problem with pngs in Windows

--- subtitld/modules/paths.py
+++ subtitld/modules/paths.py
@@ -87,8 +87,8 @@ PATH_SUBTITLD_USER_CONFIG_FILE = os.path.join(PATH_SUBTITLD_USER_CONFIG, 'subtit
 def get_graphics_path(filename=''):
     """Function to get graphics path. Windows have a problem with paths."""
     final_path = os.path.join(PATH_SUBTITLD_GRAPHICS, filename)
-    if sys.platform == 'win32' or os.name == 'nt':
-        final_path = final_path.replace('\\', '/')
+    # if sys.platform == 'win32' or os.name == 'nt':
+    #     final_path = final_path.replace('\\', '/')
     return final_path
 
 
